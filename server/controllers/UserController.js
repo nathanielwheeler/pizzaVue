71edc536b5b5c5586c69a084ced8c936fe2e8302
Valerie Wiklund <vkwiklund@gmail.com>
2019-09-13 16:28:14 -0600

    should be able to follow

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import UserService from '../services/UserService.js';
 import { Authorize } from '../middleware/authorize'
+import UserFollowing from '../services/UserFollowing.js'
 
 let _userService = new UserService().repository
+let _userFollowingService = new UserFollowing().repository
 
 
 //PUBLIC
@@ -11,6 +13,9 @@ export default class UserController {
         this.router = express.Router()
             //NOTE This route will require a query param, the client will make a request to '/api/users/find?name=Larry'
             .get('/find', this.findUserByQuery)
+            .use(Authorize.authenticated)
+            .post('/following/:id', this.followUser)
+
     }
 
     async findUserByQuery(req, res, next) {
@@ -19,7 +24,14 @@ export default class UserController {
             res.send(users)
         } catch (error) { next(error) }
     }
+    async followUser(req, res, next) {
+        try {
+            let following = await _userFollowingService
+                .create({ follower: req.session.uid, following: req.params.id })
+        } catch (error) { next(error) }
 
+
+    }
     // async findUserByQuery(req, res, next) {
     //     try {
     //         let users = await _userService.find(req.query).select('name email')
---
+++ server/services/UserFollowing.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose"
+import bcrypt from 'bcryptjs'
+let Schema = mongoose.Schema
+let ObjectId = Schema.Types.ObjectId
+
+
+
+let _schema = new Schema({
+  follower: { type: ObjectId, ref: 'User', required: true },
+  following: { type: ObjectId, ref: 'User', required: true }
+}, { timestamps: true })
+
+export default class UserFollowing {
+  constructor() {
+
+  }
+  get repository() {
+    return mongoose.model('UserFollowing', _schema)
+  }
+
+}
