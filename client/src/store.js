238f5da055d0a48448b6bfeac9ad5eef8b06f338
Tim Dohmen <dohmentim@yahoo.com>
2019-09-14 00:22:41 -0600

    debugged currently

--- client/src/store.js
+++ client/src/store.js
@@ -25,7 +25,7 @@ export default new Vuex.Store({
     users: [],
     activeUser: {},
     posts: [],
-    activePosts: {},
+    activePost: {},
     comments: [],
     userSearchResults: [],
     following: [],
